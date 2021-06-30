645a4357bb3633185f92f10c4e703ba97a79ee09
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2021-06-30 18:58:12 +0530

    xhr replaced with axios

--- functions/get-classes.js
+++ functions/get-classes.js
@@ -1,28 +1,14 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 require('dotenv').config()
+const axios = require('axios')
 var classes
 const handler = async (event) => {
     try {
-        var xhr = new XMLHttpRequest()
-        var url = `https://api.airtable.com/v0/appiaoTw3dT0mkQUJ/GoogleMeet?sort%5B0%5D%5Bfield%5D=id&sort%5B0%5D%5Bdirection%5D=asc`
-        xhr.open("GET", url, false);
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.setRequestHeader("Authorization", `Bearer ${process.env.AIRTABLE_API_KEY}`);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                classes = JSON.parse(xhr.responseText).records
-            }
-        };
-        xhr.send();
-
-
+        var url = `https://api.airtable.com/v0/appiaoTw3dT0mkQUJ/GoogleMeet?api_key=${process.env.AIRTABLE_API_KEY}&sort%5B0%5D%5Bfield%5D=id&sort%5B0%5D%5Bdirection%5D=asc`
+        const { data } = await axios.get(url)
         return {
             statusCode: 200,
-            body: JSON.stringify(classes)
+            body: JSON.stringify(data.records)
         }
-
-
-
     } catch (error) {
         return {
             statusCode: 500,
@@ -30,5 +16,4 @@ const handler = async (event) => {
         }
     }
 }
-
 module.exports = { handler }
